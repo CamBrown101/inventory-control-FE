b437b59037b89814ae49c57b42c671abe6f6ba3c
cambrown101 <cameron-brown@live.com>
2021-02-18 18:37:42 -0800

    Create Login Component and add to storybook

---
+++ src/components/Login.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Login from '../components/Login';
+
+export default {
+  title: 'Login Screen',
+  component: Login,
+};
+
+export const Default = () => <Login />;
