f58bcf4c294a499d0c6a2a89f01c539c86b60508
cambrown101 <cameron-brown@live.com>
2021-02-19 16:09:36 -0800

    add username state

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 type Props = {
   isLoggedIn: boolean;
 };
 
 const Login = ({ isLoggedIn }: Props) => {
+  const [username, setUsername] = useState('');
+
   return (
     <div>
       <h1>Login Screen</h1>
       <form>
-        <label> Username:</label>
-        <input />
+        <label for="username"> Username:</label>
+        <p>{username}</p>
+        <input
+          id="username"
+          type="text"
+          value={username}
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
         <label> Password:</label>
         <input />
         <button>Clear</button>
