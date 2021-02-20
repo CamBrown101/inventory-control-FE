b481b6b184585f08891485329594c3ac30bf8dc9
cambrown101 <cameron-brown@live.com>
2021-02-19 16:24:27 -0800

    Add login component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Login from '../src/components/Login/Login';
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Login isLoggedIn={false} />
+    </div>
+  );
 }
 
 export default App;
