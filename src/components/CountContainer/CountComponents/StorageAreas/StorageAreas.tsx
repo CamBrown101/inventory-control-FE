3a169a2c4fb37d1afa043d4134d5c6aefd12abe8
cambrown101 <cameron-brown@live.com>
2021-02-25 15:51:01 -0800

    Block out SCSS for Storage Areas

--- src/components/CountContainer/CountComponents/StorageAreas/StorageAreas.tsx
+++ src/components/CountContainer/CountComponents/StorageAreas/StorageAreas.tsx
@@ -4,27 +4,53 @@ import './StorageAreas.scss';
 const StorageAreas = () => {
   return (
     <div className="storage-areas-container">
-      <div className="count-summary-container">
-        <p className="count-summary-title">Count Summary</p>
-        <p className="dollar-value-summary">$0.00</p>
-        <p className="item-count-summary">0/100</p>
+      <div className="storage-areas-container-inner">
+        <div className="storage-areas-header">
+          <div className="count-summary-container">
+            <h3 className="count-summary-title">Count Summary</h3>
+            <div className="count-summary-values">
+              <p className="dollar-value-summary">$0.00</p>
+              <p className="item-count-summary">0/100</p>
+            </div>
+          </div>
+          <div>
+            <div className="storage-areas-title">
+              <h4>Storage Areas</h4>
+              <button>edit</button>
+            </div>
+          </div>
+          <ul className="storage-area-list">
+            <li className="storage-area-list-item">
+              <h5 className="area-name">Area Name</h5>
+              <div className="area-info">
+                <h6 className="area-dollar-value">$0.00</h6>
+                <h6 className="area-item-count">0/100</h6>
+              </div>
+            </li>
+            <li className="storage-area-list-item">
+              <h5 className="area-name">Area Name</h5>
+              <div className="area-info">
+                <h6 className="area-dollar-value">$0.00</h6>
+                <h6 className="area-item-count">0/100</h6>
+              </div>
+            </li>
+            <li className="storage-area-list-item">
+              <h5 className="area-name">Area Name</h5>
+              <div className="area-info">
+                <h6 className="area-dollar-value">$0.00</h6>
+                <h6 className="area-item-count">0/100</h6>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className="storage-areas-footer">
+          <div className="unassigned-item-container">
+            <p className="unassigned-item-title">Unassigned Items</p>
+            <p className="unassigned-item-count">20 Items</p>
+          </div>
+          <button className="finish-count-btn">Finish Count</button>
+        </div>
       </div>
-      <div className="storage-areas-title">
-        <p>Storage Areas</p>
-        <button>edit</button>
-      </div>
-      <ul className="storage-area-list">
-        <li className="storage-area-list-item">Storage Area</li>
-        <li className="storage-area-list-item">Storage Area</li>
-        <li className="storage-area-list-item">Storage Area</li>
-        <li className="storage-area-list-item">Storage Area</li>
-        <li className="storage-area-list-item">Storage Area</li>
-      </ul>
-      <div className="storage-areas-footer">
-        <p className="unassigned-item-title">Unassigned Items</p>
-        <p className="unassigned-item-count">20 Items</p>
-      </div>
-      <button className="finish-count-btn">Finish Count</button>
     </div>
   );
 };
