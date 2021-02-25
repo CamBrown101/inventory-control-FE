5f1f003307ef27a53e9f4522ab8c143b513cb462
cambrown101 <cameron-brown@live.com>
2021-02-25 15:10:34 -0800

    Create StorageAreas story

---
+++ src/components/CountContainer/CountComponents/StorageAreas/StorageAreas.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import StorageAreas from './StorageAreas';
+
+export default {
+  title: 'Count/StorageAreas',
+  component: StorageAreas,
+};
+
+export const Default = () => <StorageAreas />;
