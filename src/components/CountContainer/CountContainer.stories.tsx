9695d3f2db754b2ed487b6b45aea45c2abf5eb73
cambrown101 <cameron-brown@live.com>
2021-02-25 15:11:07 -0800

    Create CountContainer Story

---
+++ src/components/CountContainer/CountContainer.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import CountContainer from './CountContainer';
+
+export default {
+  title: 'Count/CountContainer',
+  component: CountContainer,
+};
+
+export const Default = () => <CountContainer />;
