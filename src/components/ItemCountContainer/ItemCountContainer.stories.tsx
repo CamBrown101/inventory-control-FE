e04c7ae9a8952e47be811e363e27441d9f443b40
cambrown101 <cameron-brown@live.com>
2021-03-01 17:07:47 -0800

    Boilerplate ItemCountContainer component

---
+++ src/components/ItemCountContainer/ItemCountContainer.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ItemCountContainer from './ItemCountContainer';
+
+export default {
+  title: 'Count/ItemCountContainer',
+  component: ItemCountContainer,
+};
+
+export const Default = () => <ItemCountContainer />;
---
+++ src/components/ItemCountContainer/ItemCountContainer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ItemCountContainer.scss';
+
+const ItemCountContainer = () => {
+  return (
+    <div className="item-count-container">
+      <p>Items Count Container</p>
+    </div>
+  );
+};
+
+export default ItemCountContainer;
