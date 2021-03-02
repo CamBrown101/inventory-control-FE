8d5ab474b80aa178bd54a38ac92575af61efb9bd
cambrown101 <cameron-brown@live.com>
2021-03-01 17:12:07 -0800

    Block out header

--- src/components/ItemCountContainer/ItemCountContainer.tsx
+++ src/components/ItemCountContainer/ItemCountContainer.tsx
@@ -4,6 +4,14 @@ import './ItemCountContainer.scss';
 const ItemCountContainer = () => {
   return (
     <div className="item-count-container">
+      <header>
+        <h2>Selected Area</h2>
+        <p>Last updated (Data) by (user)</p>
+        <form>
+          <button>View Filters</button>
+          <input placeholder="Search page" />
+        </form>
+      </header>
       <p>Items Count Container</p>
     </div>
   );
