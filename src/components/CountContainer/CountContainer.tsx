2d4847cf86732c6ea227b8cbcc0d260ae24c2187
cambrown101 <cameron-brown@live.com>
2021-03-03 14:26:25 -0800

    Add item container

--- src/components/CountContainer/CountContainer.tsx
+++ src/components/CountContainer/CountContainer.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import ItemCountContainer from './ItemCountContainer/ItemCountContainer';
 import StorageAreas from './StorageAreas/StorageAreas/StorageAreas';
 
 const CountContainer = () => {
   return (
     <div>
       <StorageAreas />
+      <ItemCountContainer />
     </div>
   );
 };
