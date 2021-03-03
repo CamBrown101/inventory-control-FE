9cf57a201d0bb62c46547b19bf314f8d2f60ce77
cambrown101 <cameron-brown@live.com>
2021-03-03 14:28:23 -0800

    Add scss and flex

--- src/components/CountContainer/CountContainer.tsx
+++ src/components/CountContainer/CountContainer.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './CountContainer.scss';
 import ItemCountContainer from './ItemCountContainer/ItemCountContainer';
 import StorageAreas from './StorageAreas/StorageAreas/StorageAreas';
 
 const CountContainer = () => {
   return (
-    <div>
+    <div className="count-container">
       <StorageAreas />
       <ItemCountContainer />
     </div>
