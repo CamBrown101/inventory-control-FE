7db1dd0ece9695f65a502ddc5f25c32ab6b94fbe
cambrown101 <cameron-brown@live.com>
2021-03-03 14:25:55 -0800

    Add item container

--- src/components/CountContainer/CountContainer.stories.tsx
+++ src/components/CountContainer/CountContainer.stories.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import CountContainer from './CountContainer';
+import ItemCountContainer from './ItemCountContainer/ItemCountContainer';
 
 export default {
   title: 'Count/CountContainer',
   component: CountContainer,
 };
 
-export const Default = () => <CountContainer />;
+export const Default = () => {
+  return (
+    <div>
+      <CountContainer />
+    </div>
+  );
+};
