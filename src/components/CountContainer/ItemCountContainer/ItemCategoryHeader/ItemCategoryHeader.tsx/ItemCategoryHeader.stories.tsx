b935c009f8d2f9c74f267f3f2282db917e2c031a
cambrown101 <cameron-brown@live.com>
2021-03-03 16:16:49 -0800

    Create ItemCategoryHeader component

---
+++ src/components/CountContainer/ItemCountContainer/ItemCategoryHeader/ItemCategoryHeader.tsx/ItemCategoryHeader.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ItemCountContainer from './ItemCategoryHeader';
+
+export default {
+  title: 'Count/ItemCategoryHeader',
+  component: ItemCategoryHeader,
+};
+
+export const Default = () => <ItemCategoryHeader />;
---
+++ src/components/CountContainer/ItemCountContainer/ItemCategoryHeader/ItemCategoryHeader.tsx/ItemCategoryHeader.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import 'ItemCategoryHeader.scss';
+
+const ItemCategoryHeader = () => {
+  return <div></div>;
+};
+
+export default ItemCategoryHeader;
