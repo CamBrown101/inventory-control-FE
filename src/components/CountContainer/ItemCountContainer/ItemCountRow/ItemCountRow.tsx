ae883baa2d0ff1c44dd86cad623ecb20ae4cd1f3
cambrown101 <cameron-brown@live.com>
2021-03-05 23:42:23 -0800

    Add props to ItemCountRow

--- src/components/CountContainer/ItemCountContainer/ItemCountRow/ItemCountRow.tsx
+++ src/components/CountContainer/ItemCountContainer/ItemCountRow/ItemCountRow.tsx
@@ -3,27 +3,36 @@ import './ItemCountRow.scss';
 
 type Props = {
   name: string;
-  bottleSize: number;
+  bottleSize: string;
+  bottleType: string;
   bottleValue: number;
-  areaCount: number;
+  areaCount?: number;
 };
 
-const ItemCountRow = ({ name, bottleSize, bottleValue, areaCount }: Props) => {
+const ItemCountRow = ({
+  name,
+  bottleSize,
+  bottleType,
+  bottleValue,
+  areaCount,
+}: Props) => {
   return (
     <div className="item-row-container">
       <div className="checkbox-item-name">
         <input type="checkbox" />
-        <p className="item-name">Item Name</p>
+        <p className="item-name">{name}</p>
       </div>
-      <p className="bottle-size">750ml (bottle)</p>
+      <p className="bottle-size">
+        {bottleSize} ({bottleType})
+      </p>
       <div className="dollar-value-wrapper">
         <p>value:</p>
-        <p>$49.99</p>
+        <p>${bottleValue}</p>
       </div>
       <button className="dropdown-btn">|||</button>
       <div className="count-controls">
         <button className="count-minus-button">-</button>
-        <p className="current-count">0.5</p>
+        <p className="current-count">{areaCount ? areaCount : 0}</p>
         <p>count type</p>
         <button className="count-plus-button">+</button>
       </div>
