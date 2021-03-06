aeadb632bdc0ea1d426b277c0b7d93146d3ddffc
cambrown101 <cameron-brown@live.com>
2021-03-05 23:12:58 -0800

    Create ItemCountRow stories

---
+++ src/components/CountContainer/ItemCountContainer/ItemCountRow/ItemCountRow.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ItemCountRow from './ItemCountRow';
+
+export default {
+  title: 'Count/ItemCountRow',
+  component: ItemCountRow,
+};
+
+export const Default = () => <ItemCountRow />;
