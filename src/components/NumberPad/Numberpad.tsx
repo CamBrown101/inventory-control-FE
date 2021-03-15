8d12c51052a34883a330be0232b1d746e57c3061
cambrown101 <cameron-brown@live.com>
2021-03-15 09:32:25 -0700

    Rough out html

--- src/components/NumberPad/Numberpad.tsx
+++ src/components/NumberPad/Numberpad.tsx
@@ -2,7 +2,14 @@ import React from 'react';
 import 'NumberPad.scss';
 
 function Numberpad() {
-  return <div></div>;
+  return (
+    <div className="number-pad-container">
+      <input type="number" />
+      <div className="number-buttons">
+        <button>1</button>
+      </div>
+    </div>
+  );
 }
 
 export default Numberpad;
