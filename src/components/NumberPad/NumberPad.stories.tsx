4791e25b74ffc3a409ecbd164c413c68aabd76ed
cambrown101 <cameron-brown@live.com>
2021-03-15 09:30:06 -0700

    Create Number Pad story

---
+++ src/components/NumberPad/NumberPad.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import NumberPad from './Numberpad';
+
+export default {
+  title: 'Number Pad',
+  component: NumberPad,
+};
+
+export const Default = () => <NumberPad />;
