aa51455547ff46e2778ae3ce4a2c06bcce1162dc
cambrown101 <cameron-brown@live.com>
2021-03-15 09:34:06 -0700

    Add buttons

--- src/components/NumberPad/Numberpad.tsx
+++ src/components/NumberPad/Numberpad.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'NumberPad.scss';
+import './NumberPad.scss';
 
 function Numberpad() {
   return (
@@ -7,6 +7,15 @@ function Numberpad() {
       <input type="number" />
       <div className="number-buttons">
         <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>0</button>
       </div>
     </div>
   );
