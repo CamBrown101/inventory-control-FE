ed52d4dd47557a2ab93236eda54589acaf6bc3d3
cambrown101 <cameron-brown@live.com>
2021-03-17 13:37:52 -0700

    Add click handler

--- src/components/NumberPad/Numberpad.tsx
+++ src/components/NumberPad/Numberpad.tsx
@@ -9,20 +9,34 @@ type Props = {
 function Numberpad({ value, updateFunction }: Props) {
   const [inputValue, setInputValue] = useState(`${value}`);
 
+  const handleClick = (value: number) => {
+    const valueClone = inputValue;
+    setInputValue(valueClone + value);
+    console.log(inputValue);
+  };
+
   return (
     <div className="number-pad-container">
       <input value={inputValue} type="number" />
       <div className="number-buttons">
-        <button onClick={() => {}}>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>0</button>
+        <button
+          onClick={() => {
+            handleClick(1);
+          }}>
+          1
+        </button>
+        <button
+          onClick={() => {
+            handleClick(2);
+          }}>
+          2
+        </button>{' '}
+        <button
+          onClick={() => {
+            handleClick(3);
+          }}>
+          3
+        </button>
       </div>
     </div>
   );
