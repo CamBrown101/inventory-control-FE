1f252d90e4cb3ec60156200d99f2be213fe26dde
cambrown101 <cameron-brown@live.com>
2021-03-18 20:14:16 -0700

    Add other number buttons

--- src/components/NumberPad/Numberpad.tsx
+++ src/components/NumberPad/Numberpad.tsx
@@ -43,6 +43,48 @@ function Numberpad({ value, updateFunction }: Props) {
         </button>
         <button
           onClick={() => {
+            handleClick(4);
+          }}>
+          4
+        </button>
+        <button
+          onClick={() => {
+            handleClick(5);
+          }}>
+          5
+        </button>
+        <button
+          onClick={() => {
+            handleClick(6);
+          }}>
+          6
+        </button>
+        <button
+          onClick={() => {
+            handleClick(7);
+          }}>
+          7
+        </button>
+        <button
+          onClick={() => {
+            handleClick(8);
+          }}>
+          8
+        </button>
+        <button
+          onClick={() => {
+            handleClick(9);
+          }}>
+          9
+        </button>
+        <button
+          onClick={() => {
+            handleClick(0);
+          }}>
+          0
+        </button>
+        <button
+          onClick={() => {
             handleClear();
           }}>
           clear
