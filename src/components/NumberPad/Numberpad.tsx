bbb6881698b362d54e9667356716541f05c115e1
cambrown101 <cameron-brown@live.com>
2021-03-18 20:20:12 -0700

    Add classes to buttons

--- src/components/NumberPad/Numberpad.tsx
+++ src/components/NumberPad/Numberpad.tsx
@@ -24,66 +24,77 @@ function Numberpad({ value, updateFunction }: Props) {
       <input value={inputValue} type="number" />
       <div className="number-buttons">
         <button
+          className="number-btn"
           onClick={() => {
             handleClick(1);
           }}>
           1
         </button>
         <button
+          className="number-btn"
           onClick={() => {
             handleClick(2);
           }}>
           2
         </button>{' '}
         <button
+          className="number-btn"
           onClick={() => {
             handleClick(3);
           }}>
           3
         </button>
         <button
+          className="number-btn"
           onClick={() => {
             handleClick(4);
           }}>
           4
         </button>
         <button
+          className="number-btn"
           onClick={() => {
             handleClick(5);
           }}>
           5
         </button>
         <button
+          className="number-btn"
           onClick={() => {
             handleClick(6);
           }}>
           6
         </button>
         <button
+          className="number-btn"
           onClick={() => {
             handleClick(7);
           }}>
           7
         </button>
         <button
+          className="number-btn"
           onClick={() => {
             handleClick(8);
           }}>
           8
         </button>
         <button
+          className="number-btn"
           onClick={() => {
             handleClick(9);
           }}>
           9
         </button>
         <button
+          className="number-btn"
           onClick={() => {
             handleClick(0);
           }}>
           0
         </button>
         <button
+          className="clear-btn"
           onClick={() => {
             handleClear();
           }}>
